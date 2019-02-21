3428fb19fd3197983c29d0366a3dba036300d284
Ken Snyder <ken@ken.net>
2019-02-21 10:50:05 -0800

    updated  deps

--- dist/cjs/db.d.ts
+++ dist/cjs/db.d.ts
@@ -29,12 +29,12 @@ export declare class DB extends RealTimeDB {
     protected _functions: any;
     protected app: any;
     constructor(config: IFirebaseConfig);
-    readonly auth: import("../../../../../../Users/ken/mine/forest-fire/abstracted-client/node_modules/@firebase/auth-types").FirebaseAuth;
+    readonly auth: import("@firebase/auth-types").FirebaseAuth;
     readonly database: rtdb.IFirebaseDatabase;
-    readonly firestore: import("../../../../../../Users/ken/mine/forest-fire/abstracted-client/node_modules/@firebase/firestore-types").FirebaseFirestore;
-    readonly messaging: import("../../../../../../Users/ken/mine/forest-fire/abstracted-client/node_modules/@firebase/messaging-types").FirebaseMessaging;
+    readonly firestore: import("@firebase/firestore-types").FirebaseFirestore;
+    readonly messaging: import("@firebase/messaging-types").FirebaseMessaging;
     readonly functions: any;
-    readonly storage: import("../../../../../../Users/ken/mine/forest-fire/abstracted-client/node_modules/@firebase/storage-types").FirebaseStorage;
+    readonly storage: import("@firebase/storage-types").FirebaseStorage;
     protected monitorConnection(snap: rtdb.IDataSnapshot): void;
     /**
      * connect
--- lib/EventManager.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="node" />
-import * as EventEmitter from "events";
-export declare class EventManager extends EventEmitter {
-    connection(state: boolean): void;
-}
