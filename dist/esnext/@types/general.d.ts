b86aeeda4210e407688eb378addc3da00387bd61
Ken Snyder <ken@ken.net>
2019-10-04 17:34:38 -0700

    cleaned out old files

--- dist/esnext/@types/general.d.ts
+++
@@ -1,6 +0,0 @@
-import { DB } from "../db";
-export interface IFirebaseListener {
-    id: string;
-    cb: IFirebaseConnectionCallback;
-}
-export declare type IFirebaseConnectionCallback = (db?: DB) => void;
