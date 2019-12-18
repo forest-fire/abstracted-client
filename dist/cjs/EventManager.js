48f471e276a959a403087270a89b5f0c0e6cbdbe
Ken Snyder <ken@ken.net>
2019-12-18 14:08:54 -0800

    Firebase madness

--- dist/cjs/EventManager.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const events = require("events");
-class EventManager extends events.EventEmitter {
-    connection(state) {
-        this.emit("connection", state);
-    }
-}
-exports.EventManager = EventManager;
--- dist/umd/ClientError.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class ClientError extends Error {
-    kind: string;
-    code: string;
-    constructor(message: string, classification?: string);
-}
